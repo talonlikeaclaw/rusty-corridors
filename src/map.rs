b7406d3086cb1bfa1e9b127c5ce149b10fb092a1
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-24 16:31:36 -0400

    implement map rendering function that makes walls a green '#' and floors a green '.' and reword comment

--- src/map.rs
+++ src/map.rs
@@ -17,9 +17,25 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
 }
 
-// Calculates the tile index from x and y using row-first encoding
+// Calculates the tile index from x and y using row-first striding
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
 }
