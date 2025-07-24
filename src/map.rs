2dd6b3dab5cd7b1d8228b234c93333de776575f5
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-24 16:15:09 -0400

    implement map struct and create contructor implementation

--- src/map.rs
+++ src/map.rs
@@ -6,3 +6,15 @@ pub enum TileType {
     Wall,
     Floor,
 }
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
