206538b518be72ec3b8829cb8a269631c329101e
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-25 18:28:10 -0400

    add function for filling tiles on map with specified tile to map builder implementation

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -1,8 +1,16 @@
 use crate::prelude::*;
 const NUM_ROOMS: usize = 20;
 
+// Represents a map builder
 pub struct MapBuilder {
     pub map: Map,
     pub rooms: Vec<Rect>,
     pub player_start: Point,
+}
+
+impl MapBuilder {
+    // Fills in all tiles on the map with a tile
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+    }
 }
