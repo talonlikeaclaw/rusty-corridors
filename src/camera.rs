ea1b74fe515f5b04a27521b1ff9f5fcb9bdc7c8a
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-27 13:05:21 -0400

    implement camera struct and add camera module to crate prelude

---
+++ src/camera.rs
@@ -0,0 +1,9 @@
+use crate::prelude::*;
+
+// Represents the game's Camera
+pub struct Camera {
+    pub left_x: i32,
+    pub right_x: i32,
+    pub top_y: i32,
+    pub bottom_y: i32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bracket_lib::prelude::*;
 use prelude::*;
 
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -12,6 +13,7 @@ mod prelude {
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
