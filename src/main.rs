c4e7674ea7326372a204a8cbd9569e661837ebe4
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-27 13:01:34 -0400

    add a second layer to terminal context to enable camera implementation

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,12 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Rusty Corridors")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     // Runs the main loop via context and State
