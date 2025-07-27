90759236210f085845902a8f4c4da92473cf4802
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-27 13:17:18 -0400

    implement constructor for camera implementation

--- src/camera.rs
+++ src/camera.rs
@@ -7,3 +7,15 @@ pub struct Camera {
     pub top_y: i32,
     pub bottom_y: i32,
 }
+
+impl Camera {
+    // Constuctor for the Camera implementation
+    pub fn new(player_position: Point) -> Self {
+        Self {
+            left_x: player_position.x - DISPLAY_WIDTH / 2,
+            right_x: player_position.x + DISPLAY_WIDTH / 2,
+            top_y: player_position.y - DISPLAY_HEIGHT / 2,
+            bottom_y: player_position.y + DISPLAY_HEIGHT / 2,
+        }
+    }
+}
