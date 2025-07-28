b9d6823ed03bf42047bf6d3028be3b8fdc46c110
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-27 23:42:59 -0400

    remove map's rendering function as we wont need it anymore

--- src/map.rs
+++ src/map.rs
@@ -21,38 +21,6 @@ impl Map {
         }
     }
 
-    // Renders the map within the bounds of the camera
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(1);
-        for y in camera.top_y..camera.bottom_y {
-            for x in camera.left_x..camera.right_x {
-                if self.in_bounds(Point::new(x, y)) {
-                    let idx = map_idx(x, y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('.'),
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('#'),
-                            );
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     // Checks that the location specified is within the bounds of the screen
     pub fn in_bounds(&self, point: Point) -> bool {
         point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
