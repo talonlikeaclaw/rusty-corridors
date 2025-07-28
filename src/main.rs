0dbe6ba285f5dfafe8441852ff91c2594e0ea6dd
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-27 23:52:32 -0400

    change main method to ecs using legion world, resources, and systems in main constructor

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,24 @@ mod prelude {
 
 // Represents the State of the game
 struct State {
-    map: Map,
-    camera: Camera,
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl State {
     // Constructor for the State
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            camera: Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler(),
         }
     }
 }
